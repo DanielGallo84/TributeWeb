8176a1cdb0960b26268ec212a8eb08dcc16ca1d8
Dani Gallo <danigallo@Dani.local>
2023-11-24 09:30:04 +0100

    last changes

---
+++ src/modules/js/footer.js
@@ -0,0 +1,12 @@
+let footer = document.querySelector("footer");
+
+footer.innerHTML = /* html */ `
+<div id="footer-container">
+<h2 id="copyright-footer">Jimi Hendrix © 2023</h2>
+<div id="rrss-container">
+    <a href="https://www.facebook.com/?locale=es_ES" target="blank"><img src="/images/logos/facebook-gold-icon.png" alt="facebook"></a>
+    <a href="https://twitter.com/?lang=es" target="blank"><img src="/images/logos/twitter-gold-icon.png" alt="twitter"></a>
+    <a href="https://www.instagram.com/" target="blank"><img src="/images/logos/insta-gold-icon.png" alt="instagram"></a>
+</div>
+</div>
+`
